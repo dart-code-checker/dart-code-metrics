a1039f736cf430305bc47d2e57f4b9ca28423174
Anrock <anrock623@gmail.com>
2020-03-13 15:38:09 +0300

    Validate root-folder argument
    Fixes #12

--- lib/src/cli/arguments_validation.dart
+++ lib/src/cli/arguments_validation.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:args/args.dart';
+import 'package:dart_code_metrics/src/cli/arguments_parser.dart';
 import 'package:dart_code_metrics/src/cli/arguments_validation_exceptions.dart';
 
 /// Umbrella method to run all checks
 /// throws [InvalidArgumentsException]
 void validateArguments(ArgResults arguments) {
+  checkRootFolderExistAndDirectory(arguments);
   checkPathsToAnalyzeNotEmpty(arguments);
   checkPathsExistAndDirectories(arguments);
   // TODO: check that directories to analyze are all children of root folder
@@ -24,4 +26,12 @@ void checkPathsExistAndDirectories(ArgResults arguments) {
       throw InvalidArgumentException("$p doesn't exist or isn't a directory");
     }
   });
+}
+
+void checkRootFolderExistAndDirectory(ArgResults arguments) {
+  final rootFolderPath = arguments[rootFolderName] as String;
+  if (!Directory(rootFolderPath).existsSync()) {
+    throw InvalidArgumentException(
+        'Root folder $rootFolderPath does not exist or not a directory');
+  }
 }
