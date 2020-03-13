55256482a3d1d58f36104a54ba361d4e91f789e9
Anrock <anrock623@gmail.com>
2020-03-13 15:48:29 +0300

    Validate analysis directory relative to root-folder
    Fixes #9, #10

--- lib/src/cli/arguments_validation.dart
+++ lib/src/cli/arguments_validation.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:args/args.dart';
 import 'package:dart_code_metrics/src/cli/arguments_parser.dart';
 import 'package:dart_code_metrics/src/cli/arguments_validation_exceptions.dart';
+import 'package:path/path.dart' as path;
 
 /// Umbrella method to run all checks
 /// throws [InvalidArgumentsException]
@@ -10,7 +11,6 @@ void validateArguments(ArgResults arguments) {
   checkRootFolderExistAndDirectory(arguments);
   checkPathsToAnalyzeNotEmpty(arguments);
   checkPathsExistAndDirectories(arguments);
-  // TODO: check that directories to analyze are all children of root folder
 }
 
 void checkPathsToAnalyzeNotEmpty(ArgResults arguments) {
@@ -21,9 +21,12 @@ void checkPathsToAnalyzeNotEmpty(ArgResults arguments) {
 }
 
 void checkPathsExistAndDirectories(ArgResults arguments) {
+  final rootFolderPath = arguments[rootFolderName] as String;
   arguments.rest.forEach((p) {
-    if (!Directory(p).existsSync()) {
-      throw InvalidArgumentException("$p doesn't exist or isn't a directory");
+    final absolutePath = path.join(rootFolderPath, p);
+    if (!Directory(absolutePath).existsSync()) {
+      throw InvalidArgumentException(
+          "$absolutePath doesn't exist or isn't a directory");
     }
   });
 }
