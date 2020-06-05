c3bff2367b35ba85848d60e2e400f4147e9c46a8
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-06-05 08:56:07 +0300

    add no-empty-block rule

---
+++ lib/src/rules/no_empty_block.dart
@@ -0,0 +1,50 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:dart_code_metrics/src/models/code_issue.dart';
+import 'package:dart_code_metrics/src/models/code_issue_severity.dart';
+
+import 'base_rule.dart';
+import 'rule_utils.dart';
+
+// Inspired by TSLint (https://palantir.github.io/tslint/rules/no-empty/)
+
+class NoEmptyBlockRule extends BaseRule {
+  static const _failure =
+      'Block is empty. Empty blocks are often indicators of missing code.';
+
+  const NoEmptyBlockRule()
+      : super(
+          id: 'no-empty-block',
+          severity: CodeIssueSeverity.style,
+        );
+
+  @override
+  Iterable<CodeIssue> check(
+      CompilationUnit unit, Uri sourceUrl, String sourceContent) {
+    final _visitor = _Visitor();
+
+    unit.visitChildren(_visitor);
+
+    return _visitor.emptyBlocks
+        .map((block) => createIssue(this, _failure, null, null, sourceUrl,
+            sourceContent, unit.lineInfo, block))
+        .toList();
+  }
+}
+
+class _Visitor extends RecursiveAstVisitor<Object> {
+  final _emptyBlocks = <Block>[];
+
+  Iterable<Block> get emptyBlocks => _emptyBlocks;
+
+  @override
+  void visitBlock(Block node) {
+    super.visitBlock(node);
+
+    if (node.statements.isEmpty &&
+        node.parent is! CatchClause &&
+        !(node.endToken.precedingComments?.lexeme?.contains('TODO') ?? false)) {
+      _emptyBlocks.add(node);
+    }
+  }
+}
--- lib/src/rules_factory.dart
+++ lib/src/rules_factory.dart
@@ -2,11 +2,13 @@ import 'rules/base_rule.dart';
 import 'rules/double_literal_format_rule.dart';
 import 'rules/newline_before_return.dart';
 import 'rules/no_boolean_literal_compare_rule.dart';
+import 'rules/no_empty_block.dart';
 
 const _implementedRules = {
   DoubleLiteralFormatRule(),
   NewlineBeforeReturnRule(),
   NoBooleanLiteralCompareRule(),
+  NoEmptyBlockRule(),
 };
 
 Iterable<BaseRule> get allRules => _implementedRules;
