64af1a20a3fbc655aa259ed57aac1dd413785be8
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-06-15 09:31:53 +0300

    add metricsExclude settings in AnalysisOptions

--- test/analysis_options_test.dart
+++ test/analysis_options_test.dart
@@ -75,17 +75,48 @@ linter:
     - always_put_required_named_parameters_first
 ''';
 
+const _contentWitMetricsThresholdsAndExcludes = '''
+analyzer:
+  plugins:
+    - dart_code_metrics
+  strong-mode:
+    implicit-casts: false
+    implicit-dynamic: false
+
+dart_code_metrics:
+  metrics:
+    cyclomatic-complexity: 20
+  metrics-exclude:
+    - test/**
+  rules:
+    - no-boolean-literal-compare
+
+linter:
+  rules:
+    - always_put_control_body_on_new_line
+    - always_put_required_named_parameters_first
+''';
+
 void main() {
   group('AnalysisOptions from', () {
     test('empty content', () {
-      expect(AnalysisOptions.from(null).rulesNames, isEmpty);
-      expect(AnalysisOptions.from('').rulesNames, isEmpty);
+      final configFromNull = AnalysisOptions.from(null);
+      final configFromEmptyString = AnalysisOptions.from('');
+
+      expect(configFromNull.metricsConfig, isNull);
+      expect(configFromNull.metricsExcludePatterns, isEmpty);
+      expect(configFromNull.rulesNames, isEmpty);
+
+      expect(configFromEmptyString.metricsConfig, isNull);
+      expect(configFromEmptyString.metricsExcludePatterns, isEmpty);
+      expect(configFromEmptyString.rulesNames, isEmpty);
     });
 
     test('content without metrics', () {
       final options = AnalysisOptions.from(_contentWithoutMetrics);
 
       expect(options.metricsConfig, isNull);
+      expect(options.metricsExcludePatterns, isEmpty);
       expect(options.rulesNames, isEmpty);
     });
 
@@ -94,6 +125,7 @@ void main() {
         final options = AnalysisOptions.from(_contentWitMetricsRules);
 
         expect(options.metricsConfig, isNull);
+        expect(options.metricsExcludePatterns, isEmpty);
         expect(options.rulesNames, equals(['double-literal-format']));
       });
 
@@ -101,6 +133,7 @@ void main() {
         final options = AnalysisOptions.from(_contentWitMetricsRulesAsMap);
 
         expect(options.metricsConfig, isNull);
+        expect(options.metricsExcludePatterns, isEmpty);
         expect(options.rulesNames, equals(['newline-before-return']));
       });
 
@@ -110,6 +143,17 @@ void main() {
         expect(
             options.metricsConfig.cyclomaticComplexityWarningLevel, equals(20));
         expect(options.metricsConfig.numberOfArgumentsWarningLevel, equals(4));
+        expect(options.metricsExcludePatterns, isEmpty);
+        expect(options.rulesNames, equals(['no-boolean-literal-compare']));
+      });
+
+      test('exclude define', () {
+        final options =
+            AnalysisOptions.from(_contentWitMetricsThresholdsAndExcludes);
+
+        expect(
+            options.metricsConfig.cyclomaticComplexityWarningLevel, equals(20));
+        expect(options.metricsExcludePatterns.single, equals('test/**'));
         expect(options.rulesNames, equals(['no-boolean-literal-compare']));
       });
     });
