89437c438e6139618ff9a527fd2b13f3d5f9f885
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-06-20 10:43:26 +0300

    move code examples to resource folder

--- test/scope_ast_visitor_test.dart
+++ test/scope_ast_visitor_test.dart
@@ -11,21 +11,20 @@ void main() {
     test('abstract class', () {
       final visitor = ScopeAstVisitor();
       parseFile(
-              path: p.normalize(p.absolute(
-                  './test/scope_ast_visitor_test/sample_abstract_class.dart')),
-              featureSet: FeatureSet.fromEnableFlags([]))
-          .unit
-          .visitChildren(visitor);
+          path: p.normalize(p.absolute('./test/resources/abstract_class.dart')),
+          featureSet:
+              FeatureSet.fromEnableFlags([])).unit.visitChildren(visitor);
 
       expect(visitor.declarations, isEmpty);
     });
+
     test('mixin', () {
       final visitor = ScopeAstVisitor();
       parseFile(
-          path: p.normalize(
-              p.absolute('./test/scope_ast_visitor_test/sample_mixin.dart')),
-          featureSet:
-              FeatureSet.fromEnableFlags([])).unit.visitChildren(visitor);
+              path: p.normalize(p.absolute('./test/resources/mixin.dart')),
+              featureSet: FeatureSet.fromEnableFlags([]))
+          .unit
+          .visitChildren(visitor);
 
       expect(visitor.declarations.length, equals(1));
       expect(visitor.declarations.first.declaration, isNotNull);
@@ -35,10 +34,11 @@ void main() {
     test('class with factory constructors', () {
       final visitor = ScopeAstVisitor();
       parseFile(
-          path: p.normalize(p.absolute(
-              './test/scope_ast_visitor_test/class_with_factory_constructors.dart')),
-          featureSet:
-              FeatureSet.fromEnableFlags([])).unit.visitChildren(visitor);
+              path: p.normalize(p.absolute(
+                  './test/resources/class_with_factory_constructors.dart')),
+              featureSet: FeatureSet.fromEnableFlags([]))
+          .unit
+          .visitChildren(visitor);
 
       expect(visitor.declarations.length, equals(2));
       expect(visitor.declarations.first.declaration,
