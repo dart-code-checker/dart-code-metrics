65dc2afb60b6b6e109dfd6889f021789db04216d
Anrock <anrock623@gmail.com>
2020-07-04 14:26:58 +0300

    Make MetricsAnalysisRecorder implement MetricsRecordsStore

--- lib/src/metrics_analysis_recorder.dart
+++ lib/src/metrics_analysis_recorder.dart
@@ -1,4 +1,5 @@
 import 'package:dart_code_metrics/src/metrics_records_builder.dart';
+import 'package:dart_code_metrics/src/metrics_records_store.dart';
 import 'package:dart_code_metrics/src/models/code_issue.dart';
 import 'package:dart_code_metrics/src/models/file_record.dart';
 import 'package:dart_code_metrics/src/models/function_record.dart';
@@ -11,7 +12,8 @@ import 'models/scoped_function_declaration.dart';
 
 /// Holds analysis records in format-agnostic way
 /// See [MetricsAnalysisRunner] to get analysis info
-class MetricsAnalysisRecorder implements MetricsRecordsBuilder {
+class MetricsAnalysisRecorder
+    implements MetricsRecordsBuilder, MetricsRecordsStore {
   String _fileGroupPath;
   String _relativeGroupPath;
   Map<ScopedComponentDeclaration, ComponentRecord> _componentRecords;
@@ -19,9 +21,79 @@ class MetricsAnalysisRecorder implements MetricsRecordsBuilder {
   List<CodeIssue> _issues;
 
   final _records = <FileRecord>[];
+  @override
   Iterable<FileRecord> records() => _records;
 
+  @override
+  MetricsRecordsStore recordFile(String filePath, String rootDirectory,
+      void Function(MetricsRecordsBuilder) f) {
+    if (filePath == null) {
+      throw ArgumentError.notNull('filePath');
+    }
+
+    if (f == null) {
+      throw ArgumentError.notNull('f');
+    }
+
+    _startRecordFile(filePath, rootDirectory);
+    f(this);
+    _endRecordFile();
+
+    return this;
+  }
+
+  @Deprecated('Use recordFile')
   void startRecordFile(String filePath, String rootDirectory) {
+    _startRecordFile(filePath, rootDirectory);
+  }
+
+  @Deprecated('Use recordFile')
+  void endRecordFile() {
+    _endRecordFile();
+  }
+
+  @override
+  @Deprecated('Use MetricsRecordsBuilder.recordComponent')
+  void recordComponent(
+      ScopedComponentDeclaration declaration, ComponentRecord record) {
+    _checkState();
+
+    if (declaration == null) {
+      throw ArgumentError.notNull('declaration');
+    }
+
+    _componentRecords[declaration] = record;
+  }
+
+  @override
+  @Deprecated('Use MetricsRecordsBuilder.recordFunction')
+  void recordFunction(
+      ScopedFunctionDeclaration declaration, FunctionRecord record) {
+    _checkState();
+
+    if (declaration == null) {
+      throw ArgumentError.notNull('declaration');
+    }
+
+    _functionRecords[declaration] = record;
+  }
+
+  @override
+  @Deprecated('Use MetricsRecordsBuilder.recordIssues')
+  void recordIssues(Iterable<CodeIssue> issues) {
+    _checkState();
+
+    _issues.addAll(issues);
+  }
+
+  void _checkState() {
+    if (_fileGroupPath == null) {
+      throw StateError(
+          'No record groups have been started. Use `startRecordFile` before record any data');
+    }
+  }
+
+  void _startRecordFile(String filePath, String rootDirectory) {
     if (filePath == null) {
       throw ArgumentError.notNull('filePath');
     }
@@ -39,7 +111,7 @@ class MetricsAnalysisRecorder implements MetricsRecordsBuilder {
     _issues = [];
   }
 
-  void endRecordFile() {
+  void _endRecordFile() {
     _records.add(FileRecord(
         fullPath: _fileGroupPath,
         relativePath: _relativeGroupPath,
@@ -54,43 +126,5 @@ class MetricsAnalysisRecorder implements MetricsRecordsBuilder {
     _fileGroupPath = null;
     _functionRecords = null;
     _issues = null;
-  }
-
-  @override
-  void recordComponent(
-      ScopedComponentDeclaration declaration, ComponentRecord record) {
-    _checkState();
-
-    if (declaration == null) {
-      throw ArgumentError.notNull('declaration');
-    }
-
-    _componentRecords[declaration] = record;
-  }
-
-  @override
-  void recordFunction(
-      ScopedFunctionDeclaration declaration, FunctionRecord record) {
-    _checkState();
-
-    if (declaration == null) {
-      throw ArgumentError.notNull('declaration');
-    }
-
-    _functionRecords[declaration] = record;
-  }
-
-  @override
-  void recordIssues(Iterable<CodeIssue> issues) {
-    _checkState();
-
-    _issues.addAll(issues);
-  }
-
-  void _checkState() {
-    if (_fileGroupPath == null) {
-      throw StateError(
-          'No record groups have been started. Use `startRecordFile` before record any data');
-    }
   }
 }
--- test/metrics_analysis_recorder_test.dart
+++ test/metrics_analysis_recorder_test.dart
@@ -1,4 +1,5 @@
 @TestOn('vm')
+// ignore_for_file: deprecated_member_use_from_same_package
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:dart_code_metrics/src/metrics_analysis_recorder.dart';
 import 'package:dart_code_metrics/src/models/code_issue.dart';
@@ -21,6 +22,132 @@ void main() {
   group('MetricsAnalysisRecorder', () {
     const filePath = '/home/developer/work/project/example.dart';
     const rootDirectory = '/home/developer/work/project/';
+
+    group('recordFile', () {
+      test('throws ArgumentError if called without filePath', () {
+        expect(() {
+          MetricsAnalysisRecorder().recordFile(null, null, null);
+        }, throwsArgumentError);
+      });
+
+      test('throws ArgumentError if called without function', () {
+        expect(() {
+          MetricsAnalysisRecorder().recordFile(filePath, rootDirectory, null);
+        }, throwsArgumentError);
+      });
+
+      group('Stores component records for file', () {
+        const componentName = 'simpleClass';
+
+        final simpleIdentifierMock = SimpleIdentifierMock();
+        when(simpleIdentifierMock.name).thenReturn(componentName);
+
+        final classDeclarationMock = ClassDeclarationMock();
+        when(classDeclarationMock.name).thenReturn(simpleIdentifierMock);
+
+        final record = ScopedComponentDeclaration(classDeclarationMock);
+
+        test('throws ArgumentError if called without record', () {
+          expect(() {
+            MetricsAnalysisRecorder().recordFile(filePath, rootDirectory, (b) {
+              b.recordComponent(null, null);
+            });
+          }, throwsArgumentError);
+        });
+
+        test('Stores record for file', () {
+          const componentRecord =
+              ComponentRecord(firstLine: 1, lastLine: 2, methodsCount: 3);
+
+          expect(
+              MetricsAnalysisRecorder()
+                  .recordFile(filePath, rootDirectory, (b) {
+                    b.recordComponent(record, componentRecord);
+                  })
+                  .records()
+                  .single
+                  .components,
+              containsPair(componentName, componentRecord));
+        });
+      });
+
+      group('Stores function records for file', () {
+        const functionName = 'simpleFunction';
+
+        final simpleIdentifierMock = SimpleIdentifierMock();
+        when(simpleIdentifierMock.name).thenReturn(functionName);
+
+        final functionDeclarationMock = FunctionDeclarationMock();
+        when(functionDeclarationMock.name).thenReturn(simpleIdentifierMock);
+
+        final record = ScopedFunctionDeclaration(functionDeclarationMock, null);
+
+        test('throws ArgumentError if called without record', () {
+          expect(() {
+            MetricsAnalysisRecorder().recordFile(filePath, rootDirectory, (b) {
+              b.recordFunction(null, null);
+            });
+          }, throwsArgumentError);
+        });
+
+        test('Stores record for file', () {
+          const functionRecord = FunctionRecord(
+            firstLine: 1,
+            lastLine: 2,
+            argumentsCount: 3,
+            cyclomaticComplexityLines: {},
+            linesWithCode: [],
+            operators: {},
+            operands: {},
+          );
+
+          expect(
+              MetricsAnalysisRecorder()
+                  .recordFile(filePath, rootDirectory, (b) {
+                    b.recordFunction(record, functionRecord);
+                  })
+                  .records()
+                  .single
+                  .functions,
+              containsPair(functionName, functionRecord));
+        });
+      });
+      group('Stores issues for file', () {
+        test('Aggregates issues for file', () {
+          const _issueRuleId = 'ruleId1';
+          const _issueMessage = 'first issue message';
+          const _issueCorrection = 'correction';
+          const _issueCorrectionComment = 'correction comment';
+
+          final issueRecord = MetricsAnalysisRecorder()
+              .recordFile(filePath, rootDirectory, (b) {
+                b.recordIssues([
+                  CodeIssue(
+                    ruleId: _issueRuleId,
+                    severity: CodeIssueSeverity.style,
+                    sourceSpan: SourceSpanBase(
+                        SourceLocation(1,
+                            sourceUrl: Uri.parse(filePath), line: 2, column: 3),
+                        SourceLocation(6, sourceUrl: Uri.parse(filePath)),
+                        'issue'),
+                    message: _issueMessage,
+                    correction: _issueCorrection,
+                    correctionComment: _issueCorrectionComment,
+                  ),
+                ]);
+              })
+              .records()
+              .single
+              .issues
+              .single;
+
+          expect(issueRecord.ruleId, _issueRuleId);
+          expect(issueRecord.message, _issueMessage);
+          expect(issueRecord.correction, _issueCorrection);
+          expect(issueRecord.correctionComment, _issueCorrectionComment);
+        });
+      });
+    });
 
     group('startRecordFile', () {
       test('throws ArgumentError if we call them without filePath', () {
