4dfe394a4c588f397324c38dc903960ed3d63f75
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-08-08 12:08:29 +0300

    stabialize plugin

--- lib/src/analyzer_plugin/analyzer_plugin.dart
+++ lib/src/analyzer_plugin/analyzer_plugin.dart
@@ -32,6 +32,7 @@ class MetricsAnalyzerPlugin extends ServerPlugin {
   Config _metricsConfig;
   Iterable<Glob> _metricsExclude;
   Iterable<BaseRule> _checkingCodeRules;
+  var _filesFromSetPriorityFilesRequest = <String>[];
 
   @override
   String get contactInfo => 'https://github.com/wrike/dart-code-metrics/issues';
@@ -76,16 +77,36 @@ class MetricsAnalyzerPlugin extends ServerPlugin {
       ..fileContentOverlay = fileContentOverlay;
 
     final dartDriver = contextBuilder.buildDriver(root);
-    
-    Future.forEach(dartDriver.analysisContext.contextRoot.analyzedFiles(),
-        dartDriver.getResult);
-    dartDriver
-      ..exceptions.listen((_) {
-        // TODO(dmitry): process exceptions.
-      })
-      ..results.listen(_processResult);
+    // TODO(dmitrykrutskih): Once we are ready to bump the SDK lower bound to 2.8.x, we should swap this out for `runZoneGuarded`.
+    runZoned(() {
+      dartDriver.results.listen(_processResult);
+      // ignore: avoid_types_on_closure_parameters
+    }, onError: (Object e, StackTrace stackTrace) {
+      channel.sendNotification(
+          plugin.PluginErrorParams(false, e.toString(), stackTrace.toString())
+              .toNotification());
+    });
 
     return dartDriver;
+  }
+
+  @override
+  Future<plugin.AnalysisSetContextRootsResult> handleAnalysisSetContextRoots(
+      plugin.AnalysisSetContextRootsParams parameters) async {
+    final result = await super.handleAnalysisSetContextRoots(parameters);
+    // The super-call adds files to the driver, so we need to prioritize them so they get analyzed.
+    _updatePriorityFiles();
+
+    return result;
+  }
+
+  @override
+  Future<plugin.AnalysisSetPriorityFilesResult> handleAnalysisSetPriorityFiles(
+      plugin.AnalysisSetPriorityFilesParams parameters) async {
+    _filesFromSetPriorityFilesRequest = parameters.files;
+    _updatePriorityFiles();
+
+    return plugin.AnalysisSetPriorityFilesResult();
   }
 
   @override
@@ -226,5 +247,40 @@ class MetricsAnalyzerPlugin extends ServerPlugin {
     }
 
     return null;
+  }
+
+  /// AnalysisDriver doesn't fully resolve files that are added via `addFile`; they need to be either explicitly requested
+  /// via `getResult`/etc, or added to `priorityFiles`.
+  ///
+  /// This method updates `priorityFiles` on the driver to include:
+  ///
+  /// - Any files prioritized by the analysis server via [handleAnalysisSetPriorityFiles]
+  /// - All other files the driver has been told to analyze via addFile (in [ServerPlugin.handleAnalysisSetContextRoots])
+  ///
+  /// As a result, [_processResult] will get called with resolved units, and thus all of our diagnostics
+  /// will get run on all files in the repo instead of only the currently open/edited ones!
+  void _updatePriorityFiles() {
+    final filesToFullyResolve = {
+      // Ensure these go first, since they're actually considered priority; ...
+      ..._filesFromSetPriorityFilesRequest,
+
+      // ... all other files need to be analyzed, but don't trump priority
+      for (final driver2 in driverMap.values)
+        ...(driver2 as AnalysisDriver).addedFiles,
+    };
+
+    // From ServerPlugin.handleAnalysisSetPriorityFiles
+    final filesByDriver = <AnalysisDriverGeneric, List<String>>{};
+    for (final file in filesToFullyResolve) {
+      final contextRoot = contextRootContaining(file);
+      if (contextRoot != null) {
+        // TODO(dmitrykrutskih): Which driver should we use if there is no context root?
+        final driver = driverMap[contextRoot];
+        filesByDriver.putIfAbsent(driver, () => <String>[]).add(file);
+      }
+    }
+    filesByDriver.forEach((driver, files) {
+      driver.priorityFiles = files;
+    });
   }
 }
