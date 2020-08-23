fdd61685f1d1554a0512dfa586766c4cd237b212
Dmitrii Zhifarskii <dimannich2380@gmail.com>
2020-08-23 12:47:17 +0300

    Fix import

--- lib/src/rules_factory.dart
+++ lib/src/rules_factory.dart
@@ -1,5 +1,3 @@
-import 'package:dart_code_metrics/src/rules/no_equal_then_else.dart';
-
 import 'rules/avoid_preserve_whitespace_false.dart';
 import 'rules/base_rule.dart';
 import 'rules/binary_expression_operand_order_rule.dart';
@@ -9,6 +7,7 @@ import 'rules/member_ordering.dart';
 import 'rules/newline_before_return.dart';
 import 'rules/no_boolean_literal_compare_rule.dart';
 import 'rules/no_empty_block.dart';
+import 'rules/no_equal_then_else.dart';
 import 'rules/no_magic_number_rule.dart';
 import 'rules/no_object_declaration.dart';
 import 'rules/prefer_conditional_expressions.dart';
