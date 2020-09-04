f2efb56fd9c44c4680a2efa131bb4bca644a6c6c
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-09-05 01:31:14 +0300

    add some tests

--- test/rules_factory_test.dart
+++ test/rules_factory_test.dart
@@ -6,7 +6,11 @@ void main() {
   test('getRulesById returns only required rules', () {
     expect(getRulesById({}), isEmpty);
     expect(
-        getRulesById({'double-literal-format': <String, Object>{}}).single.id,
-        equals('double-literal-format'));
+        getRulesById({
+          'double-literal-format': <String, Object>{},
+          'avoid-preserve-whitespace-false': <String, Object>{},
+          'unknown-rule': <String, Object>{},
+        }).map((rule) => rule.id),
+        equals(['avoid-preserve-whitespace-false', 'double-literal-format']));
   });
 }
