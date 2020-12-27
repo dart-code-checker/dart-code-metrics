072c3383bc46adf864e4f0e7106f1d3b48640b1a
Zalan Meggyesi <zmeggyesi@skawa.hu>
2020-12-27 09:24:26 +0100

    (#183) Update tests for rule name canonicalization

--- test/ignore_info_test.dart
+++ test/ignore_info_test.dart
@@ -28,6 +28,20 @@ void main() {
 // ignore_for_file: rule_id2, rule_id3
 ''';
 
+const _contentWithCamelCaseIgnores = '''
+// ignore_for_file: ruleId1
+
+void main() {
+  // ignore: ruleId4
+  var a = 5; // ignore: ruleId5
+
+  // ignore: ruleId6, ruleId7
+  var b = a + 5; // ignore: ruleId8, ruleId9
+}
+
+// ignore_for_file: ruleId2, ruleId3
+''';
+
 void main() {
   group('IgnoreInfo from', () {
     test('content without ignores', () {
@@ -69,6 +83,34 @@ void main() {
       expect(ignoreInfo.ignoredAt('rule_id7', 8), isTrue);
       expect(ignoreInfo.ignoredAt('rule_id8', 8), isTrue);
       expect(ignoreInfo.ignoredAt('rule_id9', 8), isTrue);
+    });
+    
+    test('content with camelcase ignores', () {
+      final parseResult = parseString(
+        content: _contentWithCamelCaseIgnores,
+        featureSet: FeatureSet.fromEnableFlags([]),
+        throwIfDiagnostics: false,
+      );
+
+      final ignoreInfo = IgnoreInfo.calculateIgnores(
+          parseResult.content, parseResult.lineInfo);
+
+      expect(ignoreInfo.hasIgnoreInfo, isTrue);
+
+      expect(ignoreInfo.ignoreRule('ruleId1'), isTrue);
+      expect(ignoreInfo.ignoreRule('ruleId2'), isTrue);
+      expect(ignoreInfo.ignoreRule('ruleId3'), isTrue);
+      expect(ignoreInfo.ignoreRule('ruleId4'), isFalse);
+
+      expect(ignoreInfo.ignoredAt('ruleId1', 5), isTrue);
+      expect(ignoreInfo.ignoredAt('ruleId2', 8), isTrue);
+      expect(ignoreInfo.ignoredAt('ruleId3', 2), isTrue);
+      expect(ignoreInfo.ignoredAt('ruleId4', 5), isTrue);
+      expect(ignoreInfo.ignoredAt('ruleId5', 5), isTrue);
+      expect(ignoreInfo.ignoredAt('ruleId6', 8), isTrue);
+      expect(ignoreInfo.ignoredAt('ruleId7', 8), isTrue);
+      expect(ignoreInfo.ignoredAt('ruleId8', 8), isTrue);
+      expect(ignoreInfo.ignoredAt('ruleId9', 8), isTrue);
     });
   });
 }
