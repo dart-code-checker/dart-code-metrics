45f83c82fdd21841ca8455838c833982a0bbb844
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-10 13:06:45 +0300

    prev

--- test/analysis_options_test.dart
+++ test/analysis_options_test.dart
@@ -1,9 +1,9 @@
 @TestOn('vm')
 import 'dart:io';
 
+import 'package:code_checker/checker.dart';
 import 'package:dart_code_metrics/src/config/analysis_options.dart';
 import 'package:dart_code_metrics/src/config/config.dart';
-import 'package:dart_code_metrics/src/utils/yaml_utils.dart';
 import 'package:test/test.dart';
 import 'package:yaml/yaml.dart';
 
