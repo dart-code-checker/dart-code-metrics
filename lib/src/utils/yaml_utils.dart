be136ca51e005cc3518faa9e45f8c24daeff4cc6
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-10 13:04:11 +0300

    use yaml utils from code checker

--- lib/src/utils/yaml_utils.dart
+++ lib/src/utils/yaml_utils.dart
@@ -1,33 +1,8 @@
 import 'dart:io';
 import 'dart:isolate';
 
+import 'package:code_checker/checker.dart';
 import 'package:yaml/yaml.dart';
-
-List<Object> yamlListToDartList(YamlList map) => List.unmodifiable(
-    List<Object>.from(map.nodes.map<Object>(yamlNodeToDartObject)));
-
-Map<String, Object> yamlMapToDartMap(YamlMap map) =>
-    Map.unmodifiable(Map<String, Object>.fromEntries(map.nodes.keys
-        .whereType<YamlScalar>()
-        .where((key) => key.value is String)
-        .map((key) => MapEntry(
-            key.value as String, yamlNodeToDartObject(map.nodes[key])))));
-
-Object yamlScalarToDartObject(YamlScalar scalar) => scalar.value as Object;
-
-Object yamlNodeToDartObject(YamlNode node) {
-  var object = Object();
-
-  if (node is YamlMap) {
-    object = yamlMapToDartMap(node);
-  } else if (node is YamlList) {
-    object = yamlListToDartList(node);
-  } else if (node is YamlScalar) {
-    object = yamlScalarToDartObject(node);
-  }
-
-  return object;
-}
 
 Future<Map<String, Object>> loadConfigFromYamlFile(File options) async {
   try {
