be6e1f64bef58c710d71d668181611097008a43f
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-01-13 11:58:58 +0300

    remove unused resource files

--- test/resources/extension.dart
+++
@@ -1,3 +0,0 @@
-extension SimpleObjectExtensions on Object {
-  T as<T>([T defaultValue]) => (this is T) ? this as T : defaultValue;
-}
--- test/resources/function.dart
+++
@@ -1,8 +0,0 @@
-String say(String from, String msg, [String device]) {
-  var result = '$from says $msg';
-  if (device != null) {
-    result = '$result with a $device';
-  }
-
-  return result;
-}
