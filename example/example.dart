a3c81b012e566216a7c0c0be3aadc060eb43b21b
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-03-14 12:55:36 +0300

    update example

--- example/example.dart
+++ example/example.dart
@@ -1,7 +1,7 @@
 import 'package:dart_code_metrics/metrics_analyzer.dart';
 import 'package:dart_code_metrics/reporters.dart';
 
-void main() {
+Future<void> main() async {
   // Get some folder you would like to analyze
   const foldersToAnalyze = ['lib', 'test'];
   // Root folder path is used to resolve relative file paths
@@ -14,26 +14,31 @@ void main() {
   final analyzer = MetricsAnalyzer(store);
 
   // Runner coordinates analyzer and store
-  final runner = MetricsAnalysisRunner(analyzer, store, foldersToAnalyze,
-      rootFolder: rootFolder);
+  final runner = MetricsAnalysisRunner(
+    analyzer,
+    store,
+    foldersToAnalyze,
+    rootFolder,
+  );
 
   // Execute run() to analyze files and collect results
-  runner.run();
+  await runner.run();
 
   // Now runner.results() contains some insights about analyzed code. Let's report it!
   // For a simple example we would report results to terminal
 
   // First of all config has to be created for a reporter
   const reporterConfig = Config(
-      cyclomaticComplexityWarningLevel: 10,
-      linesOfExecutableCodeWarningLevel: 50,
-      numberOfArgumentsWarningLevel: 4);
+    cyclomaticComplexityWarningLevel: 10,
+    linesOfExecutableCodeWarningLevel: 50,
+    numberOfArgumentsWarningLevel: 4,
+  );
 
   // Now the reporter itself
   final reporter = ConsoleReporter(reportConfig: reporterConfig);
 
   // Now pass collected analysis reports from runner to reporter and that's it
-  reporter.report(runner.results()).forEach(print);
+  (await reporter.report(runner.results())).forEach(print);
 
   // There is also JsonReporter for making machine-readable reports
   // HtmlReporter produces fancy html-documents with bells and whistles
