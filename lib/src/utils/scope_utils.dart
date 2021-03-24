b7d280f3f8750552bfbfae67c26621fd800e216d
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-03-24 22:16:04 +0300

    added scope utils

---
+++ lib/src/utils/scope_utils.dart
@@ -0,0 +1,12 @@
+import 'package:analyzer/dart/ast/ast.dart';
+
+import '../models/scoped_function_declaration.dart';
+
+/// Returns functions belonging to the passed [classNode]
+Iterable<ScopedFunctionDeclaration> classMethods(
+  Declaration classNode,
+  Iterable<ScopedFunctionDeclaration> functionDeclarations,
+) =>
+    functionDeclarations
+        .where((func) => func.enclosingDeclaration?.declaration == classNode)
+        .toList(growable: false);
