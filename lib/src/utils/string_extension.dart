502bced7046eea5ec00da410a7d546effa44d8ea
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-03-24 20:54:13 +0300

    added StringExtensions

---
+++ lib/src/utils/string_extension.dart
@@ -0,0 +1,14 @@
+final _camelRegExp = RegExp('(?=[A-Z])');
+
+/// Various useful string extensions.
+extension StringExtensions on String {
+  /// Returns a text from phrases in camel case.
+  ///
+  /// Example: print('camelCaseToText'.camelCaseToText()); => 'camel case to text'
+  String camelCaseToText() => split(_camelRegExp).join(' ').toLowerCase();
+
+  /// Returns a string with capitalized first character.
+  ///
+  /// Example: print('capitalize'.capitalize()); => 'Capitalize'
+  String capitalize() => this[0].toUpperCase() + substring(1);
+}
