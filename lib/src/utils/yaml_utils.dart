bc9aab958d5fd800d0a2f4f7dbda123c793a2a0e
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-03-25 08:58:19 +0300

    added yaml utils

---
+++ lib/src/utils/yaml_utils.dart
@@ -0,0 +1,33 @@
+import 'package:yaml/yaml.dart';
+
+/// Convert yaml [list] to Dart [List].
+List<Object> yamlListToDartList(YamlList list) =>
+    List.unmodifiable(list.nodes.map<Object>(yamlNodeToDartObject));
+
+/// Convert yaml [map] to Dart [Map].
+Map<String, Object> yamlMapToDartMap(YamlMap map) =>
+    Map.unmodifiable(Map<String, Object>.fromEntries(map.nodes.keys
+        .whereType<YamlScalar>()
+        .where((key) => key.value is String)
+        .map((key) => MapEntry(
+              key.value as String,
+              yamlNodeToDartObject(map.nodes[key]),
+            ))));
+
+/// Convert yaml [node] to Dart [Object].
+Object yamlNodeToDartObject(YamlNode node) {
+  var object = Object();
+
+  if (node is YamlMap) {
+    object = yamlMapToDartMap(node);
+  } else if (node is YamlList) {
+    object = yamlListToDartList(node);
+  } else if (node is YamlScalar) {
+    object = yamlScalarToDartObject(node);
+  }
+
+  return object;
+}
+
+/// Convert yaml [scalar] to Dart [Object].
+Object yamlScalarToDartObject(YamlScalar scalar) => scalar.value as Object;
---
+++ test/utils/yaml_utils_test.dart
@@ -0,0 +1,44 @@
+@TestOn('vm')
+import 'package:code_checker/src/utils/yaml_utils.dart';
+import 'package:test/test.dart';
+import 'package:yaml/yaml.dart';
+
+const _yamlContent = '''
+code_checker:
+  metrics:
+    cyclomatic-complexity: 20
+    number-of-methods: 8
+  metrics-exclude:
+    - test/**
+  rules:
+    - no-boolean-literal-compare
+''';
+
+void main() {
+  test('yamlMapToDartMap returns a dart map', () {
+    final configMap = yamlMapToDartMap(loadYamlNode(_yamlContent) as YamlMap);
+    expect(configMap.keys.single, equals('code_checker'));
+
+    final codeCheckerConfigMap =
+        configMap['code_checker'] as Map<String, Object>;
+    expect(
+      codeCheckerConfigMap.keys,
+      equals(['metrics', 'metrics-exclude', 'rules']),
+    );
+
+    final metricsCodeCheckerConfigMap =
+        codeCheckerConfigMap['metrics'] as Map<String, Object>;
+    expect(
+      metricsCodeCheckerConfigMap,
+      containsPair('cyclomatic-complexity', 20),
+    );
+    expect(
+      metricsCodeCheckerConfigMap,
+      containsPair('number-of-methods', 8),
+    );
+
+    final metricsExcludeCodeCheckerConfigList =
+        codeCheckerConfigMap['metrics-exclude'] as List<Object>;
+    expect(metricsExcludeCodeCheckerConfigList, equals(['test/**']));
+  });
+}
