bb723f866740124810fbafabaa3ab16f7b3cc5d4
Roman Petrov <roman.petrov@travelline.ru>
2021-05-13 09:54:49 +0300

    fix: solve LF/CRLF line endings issue when running tests

--- test/helpers/file_resolver.dart
+++ test/helpers/file_resolver.dart
@@ -5,12 +5,31 @@ import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:dart_code_metrics/src/analyzers/models/internal_resolved_unit_result.dart';
 import 'package:path/path.dart';
 
+Future<void> ensureHaveValidLineEndings(String path) async {
+  final hasCarriageReturnChar =
+      (await File(path).readAsBytes()).buffer.asUint8List().contains(0x0d);
+
+  if (hasCarriageReturnChar) {
+    throw Exception('''
+
+File: "$path"
+
+============================================================================
+All rule test input dart code files must use only LF line ending characters.
+The file contains Windows line ending character(s) so rule tests may fail.
+============================================================================
+''');
+  }
+}
+
 class FileResolver {
   static Future<InternalResolvedUnitResult> resolve(
     String filePath,
   ) async {
     final path = normalize(File(filePath).absolute.path);
     final sourceUrl = Uri.parse(path);
+
+    await ensureHaveValidLineEndings(path);
 
     // ignore: deprecated_member_use
     final parseResult = await resolveFile(path: path) as ResolvedUnitResult;
