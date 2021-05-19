7ffe3caa19d944e591908158edbf44044f4572cb
Dmitry Zhifarsky <dimannich2380@gmail.com>
2021-05-19 06:10:47 +0300

    Fix test file name (#340)
    * chore: fix imports
    
    * test: add check for a test file existence

--- test/helpers/file_resolver.dart
+++ test/helpers/file_resolver.dart
@@ -9,7 +9,13 @@ class FileResolver {
   static Future<InternalResolvedUnitResult> resolve(
     String filePath,
   ) async {
-    final path = normalize(File(filePath).absolute.path);
+    final file = File(filePath);
+
+    if (!file.existsSync()) {
+      throw StateError('Unable to find a file for the given path: $filePath');
+    }
+
+    final path = normalize(file.absolute.path);
     final sourceUrl = Uri.parse(path);
 
     // ignore: deprecated_member_use
