de4daf50ba0b1a1d1c50696f221086f46001b799
Dmitry Zhifarsky <dimannich2380@gmail.com>
2021-05-27 16:27:12 +0300

    chore: fix circular ref

--- lib/src/analyzers/lint_analyzer/models/report.dart
+++ lib/src/analyzers/lint_analyzer/models/report.dart
@@ -3,8 +3,8 @@ import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
 import 'package:source_span/source_span.dart';
 
-import '../../../../metrics_analyzer.dart';
 import '../metrics/models/metric_value.dart';
+import '../metrics/models/metric_value_level.dart';
 
 /// Represents a metrics report collected for an entity.
 @immutable
