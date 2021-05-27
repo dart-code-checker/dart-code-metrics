1758d56392108a3420e202bb03a0b436fcb6c580
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-05-27 21:59:48 +0300

    fix: rewrite resolving in CLI mode (#361)

--- lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_unnecessary_setstate/visitor.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_unnecessary_setstate/visitor.dart
@@ -78,6 +78,7 @@ class _Visitor extends RecursiveAstVisitor<void> {
     final hasSetState = visitor.setStateInvocations.isNotEmpty;
 
     visitedRestMethods[name] = hasSetState;
+
     return hasSetState;
   }
 }
