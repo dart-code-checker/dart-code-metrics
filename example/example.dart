73935460f4585dfb8c2ece2279dba5013c9ce30d
Dmitry Zhifarsky <dimannich2380@gmail.com>
2021-06-15 21:16:43 +0300

    feat: update exports before release

--- example/example.dart
+++ example/example.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:dart_code_metrics/config.dart';
-import 'package:dart_code_metrics/metrics_analyzer.dart';
+import 'package:dart_code_metrics/lint_analyzer.dart';
 
 Future<void> main() async {
   // Get some folder you would like to analyze
