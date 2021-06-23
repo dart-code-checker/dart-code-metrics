46363e0b2278bc12f710f90d41f45b6db04e9fb8
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-06-23 20:48:06 +0300

    chore: fix imports ordering

--- lib/reporters.dart
+++ lib/reporters.dart
@@ -1,2 +1,2 @@
-export 'package:dart_code_metrics/src/reporters/models/reporter.dart';
 export 'package:dart_code_metrics/src/reporters/models/file_report.dart';
+export 'package:dart_code_metrics/src/reporters/models/reporter.dart';
