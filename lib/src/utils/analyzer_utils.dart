682f2a26b262fcdb13eb1f26646050d18928c095
Dmitry Zhifarsky <dimannich2380@gmail.com>
2021-08-17 21:43:07 +0300

    chore: fix analyzer errors

--- lib/src/utils/analyzer_utils.dart
+++ lib/src/utils/analyzer_utils.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: implementation_imports
-import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:analyzer/src/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/src/dart/analysis/byte_store.dart';
 import 'package:analyzer/src/dart/analysis/file_byte_store.dart';
