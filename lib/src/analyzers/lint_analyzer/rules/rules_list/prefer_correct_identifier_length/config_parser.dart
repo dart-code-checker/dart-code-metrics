9724b22e780895cf9e65eabd004fbeeda07fd9ae
Konoshenko Vlad <konoshenko@surfstudio.ru>
2021-08-24 20:19:30 +0300

    Remove comments

--- lib/src/analyzers/lint_analyzer/rules/rules_list/prefer_correct_identifier_length/config_parser.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/prefer_correct_identifier_length/config_parser.dart
@@ -13,43 +13,33 @@ class _ConfigParser {
   static const _checkIdentifierName = 'check-variable-name';
   static const _checkArgumentName = 'check-argument-name';
 
-  //min-identifier-length
   static int? parseMinIdentifierLength(Map<String, Object> config) =>
       _parseIntConfig(config[_minIdentifierLength]);
 
-  //max-identifier-length
   static int? parseMaxIdentifierLength(Map<String, Object> config) =>
       _parseIntConfig(config[_maxIdentifierLength]);
 
-  //check-method-name
   static bool? parseCheckMethodName(Map<String, Object> config) =>
       _parseBoolConfig(config[_checkMethodName]);
 
-  //check-function-name
   static bool? parseCheckFunctionName(Map<String, Object> config) =>
       _parseBoolConfig(config[_checkFunctionName]);
 
-  //check-setters-name
   static bool? parseCheckSetters(Map<String, Object> config) =>
       _parseBoolConfig(config[_checkSetters]);
 
-  //check-getters-name
   static bool? parseCheckGetters(Map<String, Object> config) =>
       _parseBoolConfig(config[_checkGetters]);
 
-  //check-class-name
   static bool? parseCheckClassName(Map<String, Object> config) =>
       _parseBoolConfig(config[_checkClassName]);
 
-  //check-named-constructor-name
   static bool? checkConstructorName(Map<String, Object> config) =>
       _parseBoolConfig(config[_checkNamedConstructor]);
 
-  //check-variable-name
   static bool? parseCheckIdentifier(Map<String, Object> config) =>
       _parseBoolConfig(config[_checkIdentifierName]);
 
-  //check-argument-name
   static bool? checkArgumentsName(Map<String, Object> config) =>
       _parseBoolConfig(config[_checkArgumentName]);
 
