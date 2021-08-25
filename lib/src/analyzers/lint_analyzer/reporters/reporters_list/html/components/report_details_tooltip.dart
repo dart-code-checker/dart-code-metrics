fdebc9af829dcb887c98cdc21edcf09aa909e430
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-08-25 16:18:52 +0300

    refactor: improve tooltips in HTML report (#431)
    * refactor: introduce `icon` html component
    
    * refactor: introduce `report_details_tooltip` html component
    
    * refactor: improve complexity tooltips
    
    * refactor: introduce issue details tooltip

---
+++ lib/src/analyzers/lint_analyzer/reporters/reporters_list/html/components/report_details_tooltip.dart
@@ -0,0 +1,53 @@
+import 'package:html/dom.dart';
+
+import '../../../../metrics/metric_utils.dart';
+import '../../../../metrics/models/metric_value.dart';
+import '../../../../models/report.dart';
+
+Element renderDetailsTooltip(Report entityReport, String entityType) {
+  final tooltip = Element.tag('div')
+    ..classes.add('metrics-source-code__tooltip')
+    ..append(Element.tag('div')
+      ..classes.add('metrics-source-code__tooltip-title')
+      ..text = '$entityType&nbsp;stats:');
+
+  final metrics = entityReport.metrics.toList()
+    ..sort((a, b) => a.documentation.name.compareTo(b.documentation.name));
+
+  for (final metric in metrics) {
+    tooltip.append(Element.tag('p')
+      ..classes.add('metrics-source-code__tooltip-text')
+      ..append(renderDetailsTooltipMetric(metric)));
+  }
+
+  return tooltip;
+}
+
+Element renderDetailsTooltipMetric(MetricValue<num> metric) {
+  final metricName = metric.documentation.name.toLowerCase();
+  final violationLevel = metric.level.toString();
+
+  return Element.tag('div')
+    ..classes.add('metrics-source-code__tooltip-section')
+    ..append(Element.tag('p')
+      ..classes.add('metrics-source-code__tooltip-text')
+      ..append(Element.tag('a')
+        ..classes.add('metrics-source-code__tooltip-link')
+        ..attributes['href'] = documentation(metric.metricsId).toString()
+        ..attributes['target'] = '_blank'
+        ..attributes['rel'] = 'noopener noreferrer'
+        ..attributes['title'] = metricName
+        ..text = '$metricName:&nbsp;')
+      ..append(Element.tag('span')..text = metric.value.round().toString()))
+    ..append(Element.tag('p')
+      ..classes.add('metrics-source-code__tooltip-text')
+      ..append(Element.tag('span')
+        ..classes.add('metrics-source-code__tooltip-label')
+        ..text = '$metricName violation level:&nbsp;')
+      ..append(Element.tag('span')
+        ..classes.addAll([
+          'metrics-source-code__tooltip-level',
+          'metrics-source-code__tooltip-level--$violationLevel',
+        ])
+        ..text = violationLevel));
+}
---
+++ test/analyzers/lint_analyzer/reporters/reporters_list/html/components/issue_details_tooltip_test.dart
@@ -0,0 +1,45 @@
+@TestOn('vm')
+import 'package:dart_code_metrics/lint_analyzer.dart';
+import 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/issue.dart';
+import 'package:dart_code_metrics/src/analyzers/lint_analyzer/reporters/reporters_list/html/components/issue_details_tooltip.dart';
+import 'package:source_span/source_span.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Issue details tooltip:', () {
+    test(
+      'renderIssueDetailsTooltip returns tooltip dom element with details data',
+      () {
+        expect(
+          renderIssueDetailsTooltip(
+            Issue(
+              ruleId: 'ruleId',
+              documentation: Uri.parse('https://documentation.com/rule.html'),
+              location: SourceSpan(SourceLocation(0), SourceLocation(0), ''),
+              severity: Severity.warning,
+              message: 'Issue message',
+              verboseMessage: 'Issue verbose message',
+            ),
+          ).outerHtml,
+          equals(
+            '<div class="metrics-source-code__tooltip"><div class="metrics-source-code__tooltip-title">Warning: ruleId</div><p class="metrics-source-code__tooltip-section">Issue message</p><p class="metrics-source-code__tooltip-section">Issue verbose message</p><p class="metrics-source-code__tooltip-section"><a class="metrics-source-code__tooltip-link" href="https://documentation.com/rule.html" target="_blank" rel="noopener noreferrer" title="Open documentation">Open documentation</a></p></div>',
+          ),
+        );
+        expect(
+          renderIssueDetailsTooltip(
+            Issue(
+              ruleId: 'ruleId',
+              documentation: Uri.parse('https://documentation.com/rule.html'),
+              location: SourceSpan(SourceLocation(0), SourceLocation(0), ''),
+              severity: Severity.none,
+              message: 'Issue message',
+            ),
+          ).outerHtml,
+          equals(
+            '<div class="metrics-source-code__tooltip"><div class="metrics-source-code__tooltip-title">ruleId</div><p class="metrics-source-code__tooltip-section">Issue message</p><p class="metrics-source-code__tooltip-section"><a class="metrics-source-code__tooltip-link" href="https://documentation.com/rule.html" target="_blank" rel="noopener noreferrer" title="Open documentation">Open documentation</a></p></div>',
+          ),
+        );
+      },
+    );
+  });
+}
