111439d853061e9841f1176b92c950ba08aac270
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-09-19 08:48:39 +0300

    refactor: Long Method migrate on metrics values

--- lib/src/analyzers/lint_analyzer/anti_patterns/anti_patterns_list/long_method.dart
+++ lib/src/analyzers/lint_analyzer/anti_patterns/anti_patterns_list/long_method.dart
@@ -4,11 +4,13 @@ import 'package:analyzer/dart/element/type.dart';
 import '../../../../utils/node_utils.dart';
 import '../../lint_utils.dart';
 import '../../metrics/metric_utils.dart';
-import '../../metrics/metrics_list/source_lines_of_code/source_code_visitor.dart';
 import '../../metrics/metrics_list/source_lines_of_code/source_lines_of_code_metric.dart';
+import '../../metrics/models/metric_value_level.dart';
 import '../../models/entity_type.dart';
 import '../../models/internal_resolved_unit_result.dart';
 import '../../models/issue.dart';
+import '../../models/report.dart';
+import '../../models/scoped_class_declaration.dart';
 import '../../models/scoped_function_declaration.dart';
 import '../../models/severity.dart';
 import '../../rules/flutter_rule_utils.dart';
@@ -19,7 +21,7 @@ import '../pattern_utils.dart';
 class LongMethod extends ObsoletePattern {
   static const String patternId = 'long-method';
 
-  final int _sourceLinesOfCodeMetricTreshold;
+  final int? _sourceLinesOfCodeMetricTreshold;
 
   @override
   Iterable<String> get dependentMetricIds => [SourceLinesOfCodeMetric.metricId];
@@ -27,10 +29,9 @@ class LongMethod extends ObsoletePattern {
   LongMethod({
     Map<String, Object> patternSettings = const {},
     Map<String, Object> metricstTresholds = const {},
-  })  : _sourceLinesOfCodeMetricTreshold = readThreshold<int>(
+  })  : _sourceLinesOfCodeMetricTreshold = readNullableThreshold<int>(
           metricstTresholds,
           SourceLinesOfCodeMetric.metricId,
-          50,
         ),
         super(
           id: patternId,
@@ -44,35 +45,41 @@ class LongMethod extends ObsoletePattern {
         );
 
   @override
-  Iterable<Issue> legacyCheck(
+  Iterable<Issue> check(
     InternalResolvedUnitResult source,
-    Iterable<ScopedFunctionDeclaration> functions,
+    Map<ScopedClassDeclaration, Report> classMetrics,
+    Map<ScopedFunctionDeclaration, Report> functionMetrics,
   ) =>
-      functions
-          .where((function) => !_isExcluded(function))
-          .expand<Issue>((function) {
-        final visitor = SourceCodeVisitor(source.lineInfo);
-        function.declaration.visitChildren(visitor);
-
-        return [
-          if (visitor.linesWithCode.length > _sourceLinesOfCodeMetricTreshold)
-            createIssue(
-              pattern: this,
-              location: nodeLocation(
-                node: function.declaration,
-                source: source,
-              ),
-              message: _compileMessage(
-                lines: visitor.linesWithCode.length,
-                functionType: function.type,
-              ),
-              verboseMessage: _compileRecommendationMessage(
-                maximumLines: _sourceLinesOfCodeMetricTreshold,
-                functionType: function.type,
-              ),
-            ),
-        ];
-      }).toList();
+      functionMetrics.entries
+          .where((entry) => !_isExcluded(entry.key))
+          .expand((entry) => [
+                ...entry.value.metrics
+                    .where((metricValue) =>
+                        metricValue.metricsId ==
+                            SourceLinesOfCodeMetric.metricId &&
+                        metricValue.level >= MetricValueLevel.warning)
+                    .map(
+                      (metricValue) => createIssue(
+                        pattern: this,
+                        location: nodeLocation(
+                          node: entry.key.declaration,
+                          source: source,
+                        ),
+                        message: _compileMessage(
+                          lines: metricValue.value,
+                          functionType: entry.key.type,
+                        ),
+                        verboseMessage: _compileRecommendationMessage(
+                          maximumLines: _sourceLinesOfCodeMetricTreshold,
+                          functionType: entry.key.type,
+                        ),
+                      ),
+                    ),
+                if (_sourceLinesOfCodeMetricTreshold == null)
+                  // ignore: deprecated_member_use_from_same_package
+                  ..._legacyBehaviour(source, entry),
+              ])
+          .toList();
 
   bool _isExcluded(ScopedFunctionDeclaration function) {
     final declaration = function.declaration;
@@ -91,14 +98,42 @@ class LongMethod extends ObsoletePattern {
   }
 
   String _compileMessage({
-    required int lines,
+    required num lines,
     required Object functionType,
   }) =>
       'Long $functionType. This $functionType contains $lines lines with code.';
 
-  String _compileRecommendationMessage({
-    required int maximumLines,
+  String? _compileRecommendationMessage({
+    required num? maximumLines,
     required Object functionType,
   }) =>
-      "Based on configuration of this package, we don't recommend write a $functionType longer than $maximumLines lines with code.";
+      maximumLines != null
+          ? "Based on configuration of this package, we don't recommend write a $functionType longer than $maximumLines lines with code."
+          : null;
+
+  @Deprecated('Fallback for current behaviour, will be removed in 5.0.0')
+  Iterable<Issue> _legacyBehaviour(
+    InternalResolvedUnitResult source,
+    MapEntry<ScopedFunctionDeclaration, Report> entry,
+  ) =>
+      entry.value.metrics
+          .where((metricValue) =>
+              metricValue.metricsId == SourceLinesOfCodeMetric.metricId &&
+              metricValue.level == MetricValueLevel.none &&
+              metricValue.value > 50)
+          .map(
+            (metricValue) => createIssue(
+              pattern: this,
+              location: nodeLocation(
+                node: entry.key.declaration,
+                source: source,
+              ),
+              message: _compileMessage(
+                lines: metricValue.value,
+                functionType: entry.key.type,
+              ),
+              verboseMessage:
+                  'Anti pattern works in deprecated mode. Please configure ${SourceLinesOfCodeMetric.metricId} metric. For detailed information please read documentation.',
+            ),
+          );
 }
