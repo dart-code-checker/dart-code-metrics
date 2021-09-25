f2270ed9577d8ed93c3ef87d1a5bdedfe2b82fb7
Konoshenko Vlad <konoshenko@surfstudio.ru>
2021-09-25 20:25:56 +0300

    Update visitor

--- lib/src/analyzers/lint_analyzer/rules/rules_list/prefer_const_border_radius/visitor.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/prefer_const_border_radius/visitor.dart
@@ -1,25 +1,17 @@
 part of 'prefer_const_border_radius.dart';
 
 class _Visitor extends RecursiveAstVisitor<void> {
-  final _nodes = <AstNode>[];
+  final _nodes = <InstanceCreationExpression>[];
 
-  Iterable<AstNode> get nodes => _nodes;
+  Iterable<InstanceCreationExpression> get nodes => _nodes;
 
   @override
-  void visitTypeName(TypeName node) {
-    super.visitTypeName(node);
+  void visitInstanceCreationExpression(InstanceCreationExpression node) {
+    super.visitInstanceCreationExpression(node);
 
-    final borderRadiusNode = _getBorderRadiusElementDeclaration(node);
-    if (borderRadiusNode != null) {
-      _nodes.add(borderRadiusNode);
+    if (node.constructorName.beginToken.lexeme == 'BorderRadius' &&
+        node.constructorName.endToken.lexeme == 'circular') {
+      _nodes.add(node);
     }
-  }
-
-  AstNode? _getBorderRadiusElementDeclaration(TypeName element) {
-    final isBorderRadius =
-        element.parent?.beginToken.lexeme == 'BorderRadius' &&
-            element.parent?.endToken.lexeme == 'circular';
-
-    return isBorderRadius ? element.parent!.parent : null;
   }
 }
