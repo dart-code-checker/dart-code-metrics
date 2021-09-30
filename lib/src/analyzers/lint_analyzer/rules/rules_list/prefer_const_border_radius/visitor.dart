600cf83f8e7afc0c521e09c9eb0ed8e7d8540397
Denis Grafov <grafov.denis@gmail.com>
2021-09-30 19:06:57 +0300

    update visitor

--- lib/src/analyzers/lint_analyzer/rules/rules_list/prefer_const_border_radius/visitor.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/prefer_const_border_radius/visitor.dart
@@ -1,7 +1,7 @@
 part of 'prefer_const_border_radius.dart';
 
 const borderRadiusClassName = 'BorderRadius';
-const borderRadiusConstConstructorName = 'circular';
+const borderRadiusConstructorName = 'circular';
 
 class _Visitor extends RecursiveAstVisitor<void> {
   final _expressions = <InstanceCreationExpression>[];
@@ -14,9 +14,20 @@ class _Visitor extends RecursiveAstVisitor<void> {
 
     if (expression.staticType?.getDisplayString(withNullability: true) ==
             borderRadiusClassName &&
-        expression.constructorName.name?.name ==
-            borderRadiusConstConstructorName) {
-      _expressions.add(expression);
+        expression.constructorName.name?.name == borderRadiusConstructorName &&
+        expression.argumentList.arguments.length == 1) {
+      final arg = expression.argumentList.arguments.first;
+
+      if (arg is Literal) {
+        _expressions.add(expression);
+      } else if (arg is Identifier) {
+        final element = arg.staticElement;
+        if (element is PropertyAccessorElement && element.variable.isConst) {
+          _expressions.add(expression);
+        }
+      } else if (arg is BinaryExpression) {
+        // TODO(grafov): check result of binary expression
+      }
     }
   }
 }
