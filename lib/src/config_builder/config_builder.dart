5574a46f6d20b367b472fe124dfb756a87f36f2d
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-10-04 17:24:20 +0300

    feat: cleanup metrics configs

--- lib/src/config_builder/config_builder.dart
+++ lib/src/config_builder/config_builder.dart
@@ -25,26 +25,35 @@ class ConfigBuilder {
     String excludesRootFolder, {
     Iterable<Metric<num>>? classMetrics,
     Iterable<Metric<num>>? functionMetrics,
-  }) =>
-      LintAnalysisConfig(
-        prepareExcludes(config.excludePatterns, excludesRootFolder),
-        getRulesById(config.rules),
-        prepareExcludes(config.excludeForRulesPatterns, excludesRootFolder),
-        getPatternsById(config),
-        classMetrics ??
-            getMetrics(
-              config: config.metrics,
-              measuredType: EntityType.classEntity,
-            ),
-        functionMetrics ??
-            getMetrics(
-              config: config.metrics,
-              measuredType: EntityType.methodEntity,
-            ),
-        prepareExcludes(config.excludeForMetricsPatterns, excludesRootFolder),
-        config.metrics,
-        excludesRootFolder,
-      );
+  }) {
+    final patterns = getPatternsById(config);
+    final patternsDependencies = patterns
+        .map((pattern) => pattern.dependentMetricIds)
+        .expand((e) => e)
+        .toSet();
+
+    return LintAnalysisConfig(
+      prepareExcludes(config.excludePatterns, excludesRootFolder),
+      getRulesById(config.rules),
+      prepareExcludes(config.excludeForRulesPatterns, excludesRootFolder),
+      patterns,
+      classMetrics ??
+          getMetrics(
+            config: config.metrics,
+            patternsDependencies: patternsDependencies,
+            measuredType: EntityType.classEntity,
+          ),
+      functionMetrics ??
+          getMetrics(
+            config: config.metrics,
+            patternsDependencies: patternsDependencies,
+            measuredType: EntityType.methodEntity,
+          ),
+      prepareExcludes(config.excludeForMetricsPatterns, excludesRootFolder),
+      config.metrics,
+      excludesRootFolder,
+    );
+  }
 
   static UnusedFilesConfig getUnusedFilesConfigFromArgs(
     Iterable<String> excludePatterns,
