6bbf52a31b23e364bf65aae4521f41211d2293c7
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-10-22 15:01:29 +0300

    feat: reexport lint summary models

--- lib/lint_analyzer.dart
+++ lib/lint_analyzer.dart
@@ -7,5 +7,7 @@ export 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/issue.dart'
 export 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/lint_file_report.dart';
 export 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/replacement.dart';
 export 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/severity.dart';
+export 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/summary_lint_report_record.dart';
+export 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/summary_lint_report_record_status.dart';
 
 export 'package:dart_code_metrics/src/analyzers/lint_analyzer/utils/report_utils.dart';
