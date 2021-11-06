6233980db25d4ad987068cca23fb36262ecacf07
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-11-06 18:19:12 +0300

    feat: introduce file metric

---
+++ lib/src/analyzers/lint_analyzer/metrics/models/file_metric.dart
@@ -0,0 +1,43 @@
+import 'package:analyzer/dart/ast/ast.dart';
+
+import '../../models/internal_resolved_unit_result.dart';
+import '../../models/scoped_class_declaration.dart';
+import '../../models/scoped_function_declaration.dart';
+import 'metric.dart';
+import 'metric_documentation.dart';
+import 'metric_value.dart';
+import 'metric_value_level.dart';
+
+/// An interface for metrics that compute a value for a file aka compilation unit node.
+abstract class FileMetric<T extends num> extends Metric<T> {
+  /// Initialize a newly created [FileMetric].
+  const FileMetric({
+    required String id,
+    required MetricDocumentation documentation,
+    required T? threshold,
+    required MetricValueLevel Function(T, T?) levelComputer,
+  }) : super(
+          id: id,
+          documentation: documentation,
+          threshold: threshold,
+          levelComputer: levelComputer,
+        );
+
+  @override
+  bool supports(
+    AstNode node,
+    Iterable<ScopedClassDeclaration> classDeclarations,
+    Iterable<ScopedFunctionDeclaration> functionDeclarations,
+    InternalResolvedUnitResult source,
+    Iterable<MetricValue> otherMetricsValues,
+  ) =>
+      node is CompilationUnit;
+
+  @override
+  String? nodeType(
+    AstNode node,
+    Iterable<ScopedClassDeclaration> classDeclarations,
+    Iterable<ScopedFunctionDeclaration> functionDeclarations,
+  ) =>
+      null;
+}
