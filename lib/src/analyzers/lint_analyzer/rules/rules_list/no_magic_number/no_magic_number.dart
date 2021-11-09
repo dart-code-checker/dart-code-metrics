636bae969d70a4fdfba51d75b480fe0813aecc1f
Fabian Heck <f.heck@nanogiants.de>
2021-11-09 21:02:53 +0100

    fix: no-magic-number not working in array of widgets

--- lib/src/analyzers/lint_analyzer/rules/rules_list/no_magic_number/no_magic_number.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/no_magic_number/no_magic_number.dart
@@ -79,8 +79,7 @@ class NoMagicNumberRule extends CommonRule {
       ) ==
       null;
 
-  bool _isNotInsideCollectionLiteral(Literal l) =>
-      l.thisOrAncestorMatching((ancestor) => ancestor is TypedLiteral) == null;
+  bool _isNotInsideCollectionLiteral(Literal l) => !(l.parent is TypedLiteral);
 
   bool _isNotInsideConstConstructor(Literal l) =>
       l.thisOrAncestorMatching((ancestor) =>
