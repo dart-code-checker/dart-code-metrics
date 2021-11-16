9578a1acf3dbac6ee8eb942300568029b8941336
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-11-16 17:28:05 +0300

    chore: fix import

--- lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_unnecessary_type_assertions/avoid_unnecessary_type_assertions_rule.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_unnecessary_type_assertions/avoid_unnecessary_type_assertions_rule.dart
@@ -2,7 +2,7 @@ import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
-import 'package:collection/src/iterable_extensions.dart';
+import 'package:collection/collection.dart';
 
 import '../../../../../utils/node_utils.dart';
 import '../../../lint_utils.dart';
