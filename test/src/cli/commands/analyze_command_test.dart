f71a860958b81c35b9dbfcdc48597ea6056a1037
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2021-12-31 08:45:02 +0300

    fix: stabilized command usage tests (#626)
    * fix: stabilized command usage tests
    
    * docs: add missing documentation

--- test/src/cli/commands/analyze_command_test.dart
+++ test/src/cli/commands/analyze_command_test.dart
@@ -1,6 +1,7 @@
 @TestOn('vm')
 import 'package:dart_code_metrics/src/cli/cli_runner.dart';
 import 'package:dart_code_metrics/src/cli/commands/analyze_command.dart';
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 
 const _usage = 'Collect code metrics, rules and anti-patterns violations.\n'
@@ -67,10 +68,7 @@ void main() {
 
     test('should have correct usage', () {
       expect(
-        command.usage.replaceAll(
-          RegExp('defaults to "(.*?)dart-code-metrics"'),
-          'defaults to current directory',
-        ),
+        command.usage.replaceAll('"${p.current}"', 'current directory'),
         _usage,
       );
     });
