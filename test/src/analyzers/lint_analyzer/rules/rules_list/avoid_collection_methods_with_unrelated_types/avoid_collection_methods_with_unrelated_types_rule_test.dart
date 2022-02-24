1759c443a9a4715e834e9fb48f98d3f8a8eb1635
fzyzcjy <ch271828n@outlook.com>
2022-02-24 21:09:04 +0800

    add scaffold for tests

---
+++ test/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/avoid_collection_methods_with_unrelated_types_rule_test.dart
@@ -0,0 +1,13 @@
+import 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/severity.dart';
+import 'package:dart_code_metrics/src/analyzers/lint_analyzer/rules/rules_list/avoid_dynamic/avoid_dynamic_rule.dart';
+import 'package:test/test.dart';
+
+import '../../../../../helpers/rule_test_helper.dart';
+
+const _examplePath = 'avoid_collection_methods_with_unrelated_types/examples/example.dart';
+
+void main() {
+  group('AvoidCollectionMethodsWithUnrelatedTypesRule', () {
+    // TODO
+  });
+}
