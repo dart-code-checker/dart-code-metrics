708d4d62ea6dfa47be969b48af111bd76dd1ca59
fzyzcjy <ch271828n@outlook.com>
2022-02-24 21:27:48 +0800

    try to implement the rule

--- lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/avoid_collection_methods_with_unrelated_types_rule.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/avoid_collection_methods_with_unrelated_types_rule.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: public_member_api_docs
 
+import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
 
+import '../../../../../utils/node_utils.dart';
 import '../../../lint_utils.dart';
+import '../../../models/internal_resolved_unit_result.dart';
+import '../../../models/issue.dart';
 import '../../../models/severity.dart';
 import '../../models/common_rule.dart';
+import '../../rule_utils.dart';
 
 part 'visitor.dart';
 
@@ -15,10 +22,26 @@ class AvoidCollectionMethodsWithUnrelatedTypesRule extends CommonRule {
 
   AvoidCollectionMethodsWithUnrelatedTypesRule([Map<String, Object> config = const {}])
       : super(
-    id: ruleId,
-    severity: readSeverity(config, Severity.warning),
-    excludes: readExcludes(config),
-  );
+          id: ruleId,
+          severity: readSeverity(config, Severity.warning),
+          excludes: readExcludes(config),
+        );
 
-  // TODO
+  @override
+  Iterable<Issue> check(InternalResolvedUnitResult source) {
+    final visitor = _Visitor();
+
+    source.unit.visitChildren(visitor);
+
+    return visitor.expressions
+        .map((expression) => createIssue(
+              rule: this,
+              location: nodeLocation(
+                node: expression,
+                source: source,
+              ),
+              message: _warning,
+            ))
+        .toList(growable: false);
+  }
 }
--- lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/visitor.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/visitor.dart
@@ -1,5 +1,46 @@
 part of 'avoid_collection_methods_with_unrelated_types_rule.dart';
 
 class _Visitor extends RecursiveAstVisitor<void> {
-  // TODO
+  final _expressions = <Expression>[];
+
+  Iterable<Expression> get expressions => _expressions;
+
+  @override
+  void visitIndexExpression(IndexExpression node) {
+    super.visitIndexExpression(node);
+
+    final targetType = node.target?.staticType;
+    final mapKeyType = _getMapKeyType(targetType);
+    if (mapKeyType == null) {
+      return;
+    }
+
+    final mapKeyElement = mapKeyType.element;
+    if (mapKeyElement is! ClassElement) {
+      return;
+    }
+
+    final indexType = node.index.staticType;
+    if (indexType == null) {
+      return;
+    }
+
+    final indexTypeIsSubClassOfMapKeyType = indexType.asInstanceOf(mapKeyElement) != null;
+    if (!indexTypeIsSubClassOfMapKeyType) {
+      _expressions.add(node);
+    }
+  }
+
+  DartType? _getMapKeyType(DartType? type) {
+    if (type == null || !type.isDartCoreMap || type is! ParameterizedType) {
+      return null;
+    }
+
+    final typeArguments = type.typeArguments;
+    if (typeArguments.length != 2) {
+      return null;
+    }
+
+    return typeArguments.first;
+  }
 }
