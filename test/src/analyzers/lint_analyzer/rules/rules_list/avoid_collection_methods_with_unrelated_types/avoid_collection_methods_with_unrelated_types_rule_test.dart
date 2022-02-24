cc19297c6234d9fcaff48b3ad0f234a1653e0492
fzyzcjy <ch271828n@outlook.com>
2022-02-24 21:33:01 +0800

    add tests

--- test/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/avoid_collection_methods_with_unrelated_types_rule_test.dart
+++ test/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/avoid_collection_methods_with_unrelated_types_rule_test.dart
@@ -1,5 +1,5 @@
 import 'package:dart_code_metrics/src/analyzers/lint_analyzer/models/severity.dart';
-import 'package:dart_code_metrics/src/analyzers/lint_analyzer/rules/rules_list/avoid_dynamic/avoid_dynamic_rule.dart';
+import 'package:dart_code_metrics/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/avoid_collection_methods_with_unrelated_types_rule.dart';
 import 'package:test/test.dart';
 
 import '../../../../../helpers/rule_test_helper.dart';
@@ -8,6 +8,29 @@ const _examplePath = 'avoid_collection_methods_with_unrelated_types/examples/exa
 
 void main() {
   group('AvoidCollectionMethodsWithUnrelatedTypesRule', () {
-    // TODO
+    test('initialization', () async {
+      final unit = await RuleTestHelper.resolveFromFile(_examplePath);
+      final issues = AvoidCollectionMethodsWithUnrelatedTypesRule().check(unit);
+
+      RuleTestHelper.verifyInitialization(
+        issues: issues,
+        ruleId: 'avoid-collection-methods-with-unrelated-types',
+        severity: Severity.warning,
+      );
+    });
+
+    test('reports about found issues', () async {
+      final unit = await RuleTestHelper.resolveFromFile(_examplePath);
+      final issues = AvoidCollectionMethodsWithUnrelatedTypesRule().check(unit);
+
+      RuleTestHelper.verifyIssues(
+        issues: issues,
+        // TODO fill in
+        startLines: [],
+        startColumns: [],
+        locationTexts: [],
+        messages: [],
+      );
+    });
   });
 }
--- test/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/examples/example.dart
+++ test/src/analyzers/lint_analyzer/rules/rules_list/avoid_collection_methods_with_unrelated_types/examples/example.dart
@@ -1,1 +1,29 @@
-// TODO
+void main() {
+  final primitiveMap = Map<int, String>();
+  // write
+  primitiveMap[42] = "value";
+  primitiveMap["wrong_key_type"] = "value"; // LINT
+  // read
+  final a1 = primitiveMap[42];
+  final a2 = primitiveMap["wrong_key_type"]; // LINT
+
+  final inheritanceMap = Map<Animal, String>();
+  // write
+  inheritanceMap[Animal()] = "value";
+  inheritanceMap[HumanExtendsAnimal()] = "value";
+  inheritanceMap[DogImplementsAnimal()] = "value";
+  inheritanceMap[Flower()] = "value"; // LINT
+  // read
+  final b1 = inheritanceMap[Animal()];
+  final b2 = inheritanceMap[HumanExtendsAnimal()];
+  final b3 = inheritanceMap[DogImplementsAnimal()];
+  final b4 = inheritanceMap[Flower()]; // LINT
+}
+
+class Animal {}
+
+class HumanExtendsAnimal extends Animal {}
+
+class DogImplementsAnimal implements Animal {}
+
+class Flower {}
