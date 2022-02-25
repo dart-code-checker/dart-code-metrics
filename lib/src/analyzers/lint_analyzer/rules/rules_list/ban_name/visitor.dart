d81fc297b57b7104b2e1d5475aa4ddd0f9d21e17
fzyzcjy <ch271828n@outlook.com>
2022-02-25 10:20:43 +0800

    try to implement visitor

--- lib/src/analyzers/lint_analyzer/rules/rules_list/ban_name/visitor.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/ban_name/visitor.dart
@@ -2,12 +2,41 @@ part of 'ban_name_rule.dart';
 
 class _Visitor extends RecursiveAstVisitor<void> {
   final List<_BanNameConfigEntry> _entries;
+  final Map<String, _BanNameConfigEntry> _entryMap;
 
   final _nodes = <_NodeWithMessage>[];
 
   Iterable<_NodeWithMessage> get nodes => _nodes;
 
-  _Visitor(this._entries);
+  _Visitor(this._entries) : _entryMap = Map.fromEntries(_entries.map((e) => MapEntry(e.ident, e)));
+
+  @override
+  void visitSimpleIdentifier(SimpleIdentifier node) {
+    super.visitSimpleIdentifier(node);
+    _visitIdent(node, node);
+  }
+
+  @override
+  void visitPrefixedIdentifier(PrefixedIdentifier node) {
+    super.visitPrefixedIdentifier(node);
+    _visitIdent(node, node.identifier);
+    _visitIdent(node, node.prefix);
+  }
+
+  @override
+  void visitLibraryIdentifier(LibraryIdentifier node) {
+    super.visitLibraryIdentifier(node);
+    for (final component in node.components) {
+      _visitIdent(node, component);
+    }
+  }
+
+  void _visitIdent(Expression node, SimpleIdentifier ident) {
+    final name = ident.name;
+    if(_entryMap.containsKey(name)) {
+      _nodes.add(_NodeWithMessage(node, '${_entryMap[name]!.description} ($name is banned)'));
+    }
+  }
 }
 
 class _NodeWithMessage {
