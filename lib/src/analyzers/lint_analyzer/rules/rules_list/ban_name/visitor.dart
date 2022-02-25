27e338c2979ca579c3d51b761bfad49d6ee050be
fzyzcjy <ch271828n@outlook.com>
2022-02-25 10:06:13 +0800

    implement rule (without visitor)

--- lib/src/analyzers/lint_analyzer/rules/rules_list/ban_name/visitor.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/ban_name/visitor.dart
@@ -1,3 +1,18 @@
 part of 'ban_name_rule.dart';
 
-class _Visitor extends RecursiveAstVisitor<void> {}
+class _Visitor extends RecursiveAstVisitor<void> {
+  final List<_BanNameConfigEntry> _entries;
+
+  final _nodes = <_NodeWithMessage>[];
+
+  Iterable<_NodeWithMessage> get nodes => _nodes;
+
+  _Visitor(this._entries);
+}
+
+class _NodeWithMessage {
+  final Expression node;
+  final String message;
+
+  _NodeWithMessage(this.node, this.message);
+}
