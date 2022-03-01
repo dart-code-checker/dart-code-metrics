91ed3e9a19461cb14c1826dea354cc6ca8b2997f
Konoshenko Vlad <konoshenko@surfstudio.ru>
2022-03-01 17:47:35 +0300

    fix: added parameter constant check in `avoid-border-all`.

--- lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_border_all/visitor.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_border_all/visitor.dart
@@ -15,7 +15,27 @@ class _Visitor extends RecursiveAstVisitor<void> {
     if (expression.staticType?.getDisplayString(withNullability: true) ==
             _className &&
         expression.constructorName.name?.name == _borderRadiusConstructorName) {
-      _expressions.add(expression);
+      expression.argumentList.arguments.map((e) => null);
+
+      var isAllConst = true;
+
+      for (final argument in expression.argumentList.arguments) {
+        final arg = (argument as NamedExpression).expression;
+        if (arg is Literal) {
+          continue;
+        } else if (arg is MethodInvocation || arg is ConditionalExpression) {
+          isAllConst = false;
+        } else if (arg is SimpleIdentifier) {
+          final element = arg.staticElement;
+          if (element is PropertyAccessorElement && !element.variable.isConst) {
+            isAllConst = false;
+          }
+        }
+      }
+
+      if (isAllConst) {
+        _expressions.add(expression);
+      }
     }
   }
 }
