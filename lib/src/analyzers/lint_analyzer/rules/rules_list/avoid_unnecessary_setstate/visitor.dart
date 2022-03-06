14aed995a026038c7cf211cdfc6ff16cf90886af
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2022-03-06 20:42:23 +0300

    chore: fix master

--- lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_unnecessary_setstate/visitor.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/avoid_unnecessary_setstate/visitor.dart
@@ -14,6 +14,7 @@ class _Visitor extends RecursiveAstVisitor<void> {
   void visitClassDeclaration(ClassDeclaration node) {
     super.visitClassDeclaration(node);
 
+    // ignore: deprecated_member_use
     final type = node.extendsClause?.superclass2.type;
     if (type == null || !isWidgetStateOrSubclass(type)) {
       return;
