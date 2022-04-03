728b9791e0aa8f1a6c75dac2d04a9ada83cc7d06
Dmitry Zhifarsky <dimannich2380@gmail.com>
2022-04-03 13:26:10 +0400

    feat: add static code diagnostic prefer-commenting-analyzer-ignores (#776)
    * feat: add static code diagnostic prefer-commenting-analyzer-ignores
    
    * fix: add new rule to factory
    
    * test: update suppression test
    
    Co-authored-by: Dmitry Krutskikh <dmitry.krutskikh@gmail.com>

---
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/prefer_commenting_analyzer_ignores/prefer_commenting_analyzer_ignores.dart
@@ -0,0 +1,41 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/token.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/source/line_info.dart';
+
+import '../../../../../utils/node_utils.dart';
+import '../../../lint_utils.dart';
+import '../../../models/internal_resolved_unit_result.dart';
+import '../../../models/issue.dart';
+import '../../../models/severity.dart';
+import '../../models/common_rule.dart';
+import '../../rule_utils.dart';
+
+part 'visitor.dart';
+
+class PreferCommentingAnalyzerIgnores extends CommonRule {
+  static const String ruleId = 'prefer-commenting-analyzer-ignores';
+
+  static const _warning = 'Prefer commenting analyzer ignores.';
+
+  PreferCommentingAnalyzerIgnores([Map<String, Object> config = const {}])
+      : super(
+          id: ruleId,
+          severity: readSeverity(config, Severity.warning),
+          excludes: readExcludes(config),
+        );
+
+  @override
+  Iterable<Issue> check(InternalResolvedUnitResult source) {
+    final visitor = _Visitor(source.lineInfo)..checkComments(source.unit.root);
+
+    return visitor.comments.map((comment) => createIssue(
+          rule: this,
+          location: nodeLocation(
+            node: comment,
+            source: source,
+          ),
+          message: _warning,
+        ));
+  }
+}
