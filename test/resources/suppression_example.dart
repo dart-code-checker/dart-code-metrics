6a7bdbe05c369f563889f285d011851e11372e50
Dmitry Zhifarsky <dimannich2380@gmail.com>
2022-04-03 12:28:49 +0400

    test: update suppression test

--- test/resources/suppression_example.dart
+++ test/resources/suppression_example.dart
@@ -4,7 +4,8 @@ void main() {
   // ignore: rule_id4
   const a = 5; // ignore: RULE_ID5
 
-  // ignore:rule_id6 ,rule_id7
+  // ignore comment
+  // ignore:rule_id6 ,rule_id7, some comment about ignores
   const b = a + 5; // ignore: RULE_ID8, rule_id9, unused_local_variable
 }
 
