a138b33450e943319bfbd404dec8856dcb9c1fd1
Dmitry Zhifarsky <dimannich2380@gmail.com>
2022-05-01 16:25:45 +0400

    test: update test case

--- test/src/analyzers/lint_analyzer/rules/rules_list/avoid_non_ascii_symbols/examples/example.dart
+++ test/src/analyzers/lint_analyzer/rules/rules_list/avoid_non_ascii_symbols/examples/example.dart
@@ -2,7 +2,7 @@ void main() {
   final english = 'hello';
   final chinese = 'hello 汉字'; // LINT
   final russian = 'hello привет'; // LINT
-  final someGenericSymbols ='!@#$%^';
+  final someGenericSymbols = '!@#${english}%^';
   final nonAsciiSymbols = '#!$_&-  éè  ;∞¥₤€'; // LINT
   final misspelling = 'inform@tiv€'; // LINT
 }
