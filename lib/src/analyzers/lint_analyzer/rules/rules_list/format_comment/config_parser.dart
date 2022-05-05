7624995be5dbd318f4260f5c3dbface0f8409421
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-05-05 12:00:48 +0800

    :zap: Cast as non nullable as we checked before

--- lib/src/analyzers/lint_analyzer/rules/rules_list/format_comment/config_parser.dart
+++ lib/src/analyzers/lint_analyzer/rules/rules_list/format_comment/config_parser.dart
@@ -6,7 +6,7 @@ class _ConfigParser {
   static Iterable<RegExp> getIgnoredPatterns(Map<String, Object> config) =>
       config[_ignoredPatternsConfig] is Iterable
           ? List<String>.from(
-              config[_ignoredPatternsConfig] as Iterable? ?? const <String>[],
+              config[_ignoredPatternsConfig] as Iterable,
             ).map((stringPattern) => RegExp(stringPattern))
           : const [];
 }
